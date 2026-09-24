c6cd6e11189968bd2d82be32c8399b20de9d62c5
Eng. Ahmed Al-Qasabi <ahmedalqasabi65@gmail.com>
2026-09-24 15:46:25 +0300

    Program to add two integers.

--- Unit 2/Assignment 1/EX_3.c
+++ Unit 2/Assignment 1/EX_3.c
@@ -1,15 +1,10 @@
 /*
- ============================================================================
- Name        : Assignment.c
- Author      : Ahmed Mohamed Al-Qasabi
- Version     :
- Copyright   : Copyright (c) ${date:2026} Ahmed Mohamed Bassiouni Al-Qasabi
- Description : Hello World in C, Ansi-style
- ============================================================================
+ * File: EX_3.c
+ * Author: Ahmed Mohamed Al-Qasabi
+ * Description: Program to add two integers.
  */
 
-
-														/* C Program to Add Two Integers */
+/* C Program to Add Two Integers */
 
 #include <stdio.h>
 
