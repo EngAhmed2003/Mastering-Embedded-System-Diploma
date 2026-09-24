450e9b0e355cbb9bc7b0265ff530716a27be2f2a
Eng. Ahmed Al-Qasabi <ahmedalqasabi65@gmail.com>
2026-09-24 15:46:29 +0300

    Program to multiply two floating-point numbers.

--- Unit 2/Assignment 1/EX_4.c
+++ Unit 2/Assignment 1/EX_4.c
@@ -1,15 +1,10 @@
 /*
- ============================================================================
- Name        : Assignment.c
- Author      : Ahmed Mohamed Al-Qasabi
- Version     :
- Copyright   : Copyright (c) ${date:2026} Ahmed Mohamed Bassiouni Al-Qasabi
- Description : Hello World in C, Ansi-style
- ============================================================================
+ * File: EX_4.c
+ * Author: Ahmed Mohamed Al-Qasabi
+ * Description: Program to multiply two floating-point numbers.
  */
 
-
-														/* C Program to Multiply two Floating Point Numbers */
+/* C Program to Multiply two Floating Point Numbers */
 
 #include <stdio.h>
 
