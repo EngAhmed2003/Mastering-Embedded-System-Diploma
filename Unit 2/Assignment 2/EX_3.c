02910914572a0097ff6e832bf153f3c483c7a5ab
Eng. Ahmed Al-Qasabi <ahmedalqasabi65@gmail.com>
2026-09-24 15:46:47 +0300

    Program to find the largest number among three numbers.

--- Unit 2/Assignment 2/EX_3.c
+++ Unit 2/Assignment 2/EX_3.c
@@ -1,15 +1,10 @@
 /*
- ============================================================================
- Name        : Assignment.c
- Author      : Ahmed Mohamed Al-Qasabi
- Version     :
- Copyright   : Copyright (c) ${date:2026} Ahmed Mohamed Bassiouni Al-Qasabi
- Description : Hello World in C, Ansi-style
- ============================================================================
+ * File: EX_3.c
+ * Author: Ahmed Mohamed Al-Qasabi
+ * Description: Program to find the largest number among three numbers.
  */
 
-
-														/* C Program to Find the Largest Number Among Three Numbers */
+/* C Program to Find the Largest Number Among Three Numbers */
 
 #include <stdio.h>
 
